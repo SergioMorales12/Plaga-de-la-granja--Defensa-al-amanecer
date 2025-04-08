e76c2a3fc70dd14890d77dcd52c67a722c18dd64
smorcor744 <148891340+smorcor744@users.noreply.github.com>
2025-04-08 10:58:24 +0200

    Tower activa

--- scenes/enemi/cuervo1.gd
+++ scenes/enemi/cuervo1.gd
@@ -4,6 +4,17 @@ extends PathFollow2D
 @export var damage = 0
 @export var live = 0
 
+func _ready():
+	var area = Area2D.new()
+	var collision_shape = CollisionShape2D.new()
+	var shape = CircleShape2D.new()
+	shape.radius = 10
+	collision_shape.shape = shape
+	
+	area.add_child(collision_shape)
+	add_child(area)
+	area.add_to_group("enemi")
+
 func _process(delta: float) -> void:
 	loop_movement(delta)
 	
--- scripts/espanta_pajaros.gd
+++ scripts/espanta_pajaros.gd
@@ -1,23 +1,27 @@
 extends Node2D
 
-@export var original_sprite: Texture
-@export var alternate_sprite: Texture
+@export var hp = 0
+@export var damage = 0
+@export var speed = 0
 
 func _ready():
-	$Area2D.connect("body_entered", Callable(self, "_on_body_entered"))
-	$Area2D.connect("body_exited", Callable(self, "_on_body_exited"))
+	$Area2D.monitoring = true
 
-func _on_body_entered(body):
-	if body.is_in_group("enemi"):
-		$Sprite.texture = alternate_sprite
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("enemi"):
+		print("Es un enemigo")
+		$sleep.visible = false
+		$active.visible = true
 
-func _on_body_exited(body):
-	if body.is_in_group("enemigo"):
-		var overlapping_bodies = $Area2D.get_overlapping_bodies()
+
+func _on_area_exited(area: Area2D) -> void:
+	if area.is_in_group("enemi"):
+		var overlapping_bodies = $Area2D.get_overlapping_areas()
 		var enemy_present = false
 		for b in overlapping_bodies:
-			if b.is_in_group("enemigo"):
+			if b.is_in_group("enemi"):
 				enemy_present = true
 				break
 		if not enemy_present:
-			$Sprite.texture = original_sprite
+			$sleep.visible = true
+			$active.visible = false
